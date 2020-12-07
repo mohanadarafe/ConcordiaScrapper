15f868d7a071fdde2a66958e884e0fefb7e6e6bc
mohanadarafe <marafe@live.com>
2020-12-07 12:36:55 -0500

    export results JSON file with URL & text content

--- ConcordiaScrapper/ConcordiaScrapper/spiders/concordia_spider.py
+++ ConcordiaScrapper/ConcordiaScrapper/spiders/concordia_spider.py
@@ -1,12 +1,13 @@
-import scrapy, logging, os, shutil
+import scrapy, logging, os, shutil, string
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.exceptions import CloseSpider
 from scrapy.crawler import CrawlerProcess
+from bs4 import BeautifulSoup
+from nltk import word_tokenize
 
 class ConcordiaScrapper(CrawlSpider):
-
-    file_limit = 100
+    file_limit = 100 #default limit = 100
     def __init__(self, limit=100, **kwargs):
         super().__init__(**kwargs)
         self.file_limit = limit
@@ -15,11 +16,12 @@ class ConcordiaScrapper(CrawlSpider):
     start_urls = [
         'https://concordia.ca/',
     ]
+    visited_urls = set()
     process = CrawlerProcess(settings={
         "CONCURRENT_REQUESTS": 1,
         "ROBOTSTXT_OBEY": True,
         "CLOSESPIDER_ITEMCOUNT": int(file_limit),
-        "LOG_LEVEL": 'INFO',
+        "LOG_LEVEL": "INFO"
     })
     rules = (
         Rule(LinkExtractor(deny=(r'^(?!https://www.concordia.ca).+', )),
@@ -32,7 +34,19 @@ class ConcordiaScrapper(CrawlSpider):
             raise CloseSpider("Limit reached.")
 
         url = response.url
+        if url in self.visited_urls:
+            self.logger.info(f'Already scrapped: {url}')
+            yield
+        else:
+            self.visited_urls.add(url)
+
         self.logger.info(f'Scrapping: {url}')
-        with open(f'spiders/concordia_results/{self.docNumber}.html', 'wb') as f:
-            f.write(response.body)
+        soup = BeautifulSoup(response.body, "lxml")
+        text = word_tokenize(soup.get_text())
+        text = [token for token in text if token not in string.punctuation]
         self.docNumber += 1
+
+        yield {
+            "url": url,
+            "text": text
+        }
