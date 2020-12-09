db9bcfcd112f704c05b062d2fcb4ca7fb8e7b974
Mohanad <marafe@live.com>
2020-12-09 17:48:59 -0500

    fix results folder error & project structure

--- run.py
+++ run.py
@@ -1,29 +1,33 @@
-import os
+import os, shutil
 from scrapy.crawler import CrawlerProcess
-from spiders.test_crawler import TestScrapper
+from spiders.concordia_crawler import ConcordiaScrapper
 
 upperBound = input("Please enter an upper bound: ")
 while (not upperBound.isnumeric() or int(upperBound) < 0):
     print("Invalid input. Please enter a positive number.")
     upperBound = input("Please enter an upper bound: ")
 
-if os.path.isfile("results/test_result.json"):
-    os.remove("results/test_result.json")
+if os.path.isdir("results"):
+        shutil.rmtree("results")
 
-os.chdir('results')
+os.makedirs("results")
+os.chdir("results")
+
 process = CrawlerProcess(settings={
     "CONCURRENT_REQUESTS": 1,
     "ROBOTSTXT_OBEY": True,
     "CLOSESPIDER_ITEMCOUNT": upperBound,
     "LOG_LEVEL": "INFO",
     "FEEDS": {
-        'test_result.json': {
+        'result.json': {
             'format': 'json',
             'encoding': 'utf8',
             'store_empty': False
         },
     },
 })
-TestScrapper.file_limit = upperBound
-process.crawl(TestScrapper)
+ConcordiaScrapper.file_limit = upperBound
+process.crawl(ConcordiaScrapper)
 process.start()
+assert os.path.isfile("result.json"), "Results file does not exist!"
+assert os.path.isfile("visited_urls.json"), "URLs file does not exist!"
